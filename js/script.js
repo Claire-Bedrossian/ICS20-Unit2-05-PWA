0a91d108368a4afd3dda81615a9ca1f27da81740
Claire-Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-25 17:49:21 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,14 +10,34 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-Unit2-05-PWA/sw.js", {
+    scope: "/ICS20-Unit2-05-PWA/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function calculates the user pay and tax deductions
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  // input
+  const hours = parseFloat(document.getElementById("hours-worked").value)
+  const hourlyPay = parseFloat(document.getElementById("hourly-pay").value)
+  const TAX_RATE = 0.18
+
+  //process
+
+  const grossPay = hours * hourlyPay
+  const deductions = TAX_RATE * grossPay
+  const afterDeductions = grossPay - TAX_RATE * grossPay
+
+  // output
+  document.getElementById(
+    "grossPay"
+  ).innerHTML = `Your pay will be: $ ${grossPay.toFixed(2)}`
+  document.getElementById(
+    "deductions"
+  ).innerHTML = `The government will take: $ ${deductions.toFixed(2)}`
+  document.getElementById(
+    "afterDeductions"
+  ).innerHTML = `Your pay after deductions is: $ ${afterDeductions.toFixed(2)}`
 }
